d7c781b0f7481c908ca9fbd3d8a8151b7a3d290b
C. Daniel Sanchez R <cdsanchezr@unal.edu.co>
2014-10-08 03:59:30 -0500

    App funcional. Ya envía datos al servidor.

--- Robotica/app/src/main/java/com/example/juancamilo/robotica/MainActivity.java
+++ Robotica/app/src/main/java/com/example/juancamilo/robotica/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.juancamilo.robotica;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -16,7 +19,13 @@ public class MainActivity extends Activity {
     private ToggleButton tg1= null;
     private ToggleButton tg2= null;
     private ToggleButton tg3 = null;
+    private EditText ip = null;
     private ToggleButton connect = null;
+
+    protected char B1,B2,B3 = 0;
+    protected char S1,S2 = 127; //la mitad de 254
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +35,16 @@ public class MainActivity extends Activity {
         tg1= (ToggleButton) findViewById(R.id.toggleButton);
         tg2= (ToggleButton) findViewById(R.id.toggleButton2);
         tg3= (ToggleButton) findViewById(R.id.toggleButton3);
+        ip = (EditText)findViewById(R.id.editTextIP);
         connect= (ToggleButton) findViewById(R.id.connect);
         connect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean connected) {
             // Acá debe ir el intento de conexión al servidor
-                if(connected){
-
-                }
+            if(connected){
+                Log.i("Antes de conectar: ","IP: "+ip.getText().toString());
+                new connectTask().execute();
+            }
             }
         });
         aceleracion.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -45,7 +56,8 @@ public class MainActivity extends Activity {
                 acelText2.setText(i+"");
                 ToggleButton connect=(ToggleButton) findViewById(R.id.connect);
                 if(connect.isChecked()){
-
+                    S1 = (char) i;
+                    generarMensaje();
                 }
             }
 
@@ -68,7 +80,8 @@ public class MainActivity extends Activity {
                 velText2.setText(progress+"");
                 ToggleButton connect=(ToggleButton) findViewById(R.id.connect);
                 if(connect.isChecked()){
-
+                    S2 = (char) progress;
+                    generarMensaje();
                 }
             }
 
@@ -89,7 +102,8 @@ public class MainActivity extends Activity {
                 toggleText.setText( (b?"ON":"OFF"));
                 ToggleButton connect=(ToggleButton) findViewById(R.id.connect);
                 if(connect.isChecked()){
-
+                    B1= (char) (b ? 1:0);
+                    generarMensaje();
                 }
             }
         });tg2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -99,17 +113,19 @@ public class MainActivity extends Activity {
                 toggleText.setText((b?"ON":"OFF"));
                 ToggleButton connect=(ToggleButton) findViewById(R.id.connect);
                 if(connect.isChecked()){
-
+                    B2= (char) (b ? 1:0);
+                    generarMensaje();
                 }
             }
         });tg3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                TextView toggleText=(TextView) findViewById(R.id.toggle3);
-                toggleText.setText((b?"ON":"OFF"));
-                ToggleButton connect=(ToggleButton) findViewById(R.id.connect);
-                if(connect.isChecked()){
-
+                TextView toggleText = (TextView) findViewById(R.id.toggle3);
+                toggleText.setText((b ? "ON" : "OFF"));
+                ToggleButton connect = (ToggleButton) findViewById(R.id.connect);
+                if (connect.isChecked()) {
+                    B3= (char) (b ? 1:0);
+                    generarMensaje();
                 }
             }
         });
@@ -133,5 +149,62 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /*************************************************************/
+
+    public String generarMensaje(){
+
+
+        String message= Character.toString((char) 126)+
+                        Character.toString(B1)+
+                        Character.toString(B2)+
+                        Character.toString(B3)+
+                        Character.toString(S1)+
+                        Character.toString(S2);
+
+
+        TextView data = (TextView) findViewById(R.id.textView8);
+        data.setText("Datos: "+B1+B2+B3+S1+S2);
+
+        Conection conn = Conection.getInstance();
+
+        conn.enviarMensaje(message);
+
+
+
+
+        return null;
+    }
+
+    //Clase interna para manejar la conexión de manea asincrónica
+    public class connectTask extends AsyncTask<String,String,Conection> {
+
+
+
+        @Override
+        protected Conection doInBackground(String... laIP) {
+
+            Conection conn = Conection.getInstance();
+
+            conn.addListener(new OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+
+
+            conn.conectar(ip.getText().toString());
+
+
+            return null;
+        }
+
+
+
     }
 }
